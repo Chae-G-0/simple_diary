ac97ccdf34369573017c596849891c47be4ff575
Chae-G-0 <corkdud@naver.com>
2022-12-07 21:06:03 +0900

    221207

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummuyList = [
+  {
+    id: 1,
+    author: "chae",
+    content: "hi",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "a",
+    content: "chu",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "b",
+    content: "bye",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "c",
+    content: "hello",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "d",
+    content: "hungry",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummuyList} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,7 +18,14 @@ const DiaryEditor = () => {
   };
 
   const handleSubnit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
     alert("저장 성공");
   };
 
@@ -24,6 +34,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -31,13 +42,14 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
         />
       </div>
       <div>
-        오늘의 감정 점수 : 
+        오늘의 감정 점수 :
         <select
           name="emotion"
           value={state.emotion}
