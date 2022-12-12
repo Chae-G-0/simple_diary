892d404045ddeca0a31a937a675cb8ba7d560aaa
Chae-G-0 <corkdud@naver.com>
2022-12-12 22:28:45 +0900

    221212

--- src/App.js
+++ src/App.js
@@ -1,50 +1,72 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummuyList = [
-  {
-    id: 1,
-    author: "chae",
-    content: "hi",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "a",
-    content: "chu",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "b",
-    content: "bye",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "c",
-    content: "hello",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 5,
-    author: "d",
-    content: "hungry",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummuyList = [
+//   {
+//     id: 1,
+//     author: "chae",
+//     content: "hi",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "a",
+//     content: "chu",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "b",
+//     content: "bye",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "c",
+//     content: "hello",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 5,
+//     author: "d",
+//     content: "hungry",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+  const dateId = useRef(0);
+  const onCreate = (author, content, emotion) => {
+    const create_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      create_date,
+      id: dateId.current,
+    };
+    dateId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제 되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummuyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-const DiaryItem = ({ author, content, emotion, created_date, id }) => {
+const DiaryItem = ({
+  onDelete,
+  author,
+  content,
+  emotion,
+  created_date,
+  id,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -10,7 +17,17 @@ const DiaryItem = ({ author, content, emotion, created_date, id }) => {
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-        <div className="content">{content} </div>
+      <div className="content">{content} </div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id + 1}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
