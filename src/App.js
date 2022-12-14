ab0cef83c220e3d5feae668e818922918bc6d89c
Chae-G-0 <corkdud@naver.com>
2022-12-14 19:55:36 +0900

    221214

--- src/App.js
+++ src/App.js
@@ -1,50 +1,34 @@
-import { useRef, useState } from "react";
+import { useMemo } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import LifeCycle from "./LifeCycle";
-
-// const dummuyList = [
-//   {
-//     id: 1,
-//     author: "chae",
-//     content: "hi",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "a",
-//     content: "chu",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "b",
-//     content: "bye",
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: "c",
-//     content: "hello",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 5,
-//     author: "d",
-//     content: "hungry",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-// ];
 
 function App() {
   const [data, setData] = useState([]);
   const dateId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_date: new Date().getTime(),
+        id: dateId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const create_date = new Date().getTime();
     const newItem = {
@@ -65,13 +49,31 @@ function App() {
   };
 
   const onEdit = (targetId, newContent) => {
-    setData(data.map((it)=>it.id === targetId ? {...it, content: newContent} : it))
-  }
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
 
   return (
     <div className="App">
-      <LifeCycle/>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length} </div>
+      <div>기분 좋은 일기 : {goodCount} </div>
+      <div>기분 나쁜 일기 : {badCount} </div>
+      <div>기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
